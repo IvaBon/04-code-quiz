98fe56d94194a18ed2da58dd23621bb50e1537e4
ivan bonilla <ivanbonilla02@yahoo.com>
2022-09-20 23:52:31 -0500

    worked on show question function

--- assests/script.js
+++ assests/script.js
@@ -11,63 +11,76 @@ var currentscore=0;
 var secondsRemaining=120;
 var startButton = document.getElementById('startButton');
 var startPage = document.getElementById('startpage');
-// var currQuestionObj = questions[i]
-var h2
+
 startButton.addEventListener('click',startQuiz);
 
 
 function startQuiz(){
     console.log('Started');
     startButton,startPage.classList.add('hide');
-    
+    showAQuestion();
 }
+
+
+
 // how do i get each item out of the array
 function showAQuestion(){
     
+    var section= document.createElement('section');
+    var questionTitle= document.createElement('h2');
+    var ul=document.createElement('ul');
+    var choice1=document.createElement('li');
+    var choice2=document.createElement('li');
+    var choice3=document.createElement('li');
+    var choice4=document.createElement('li');
+
+    document.body.appendChild(section);
+    section.appendChild(questionTitle);
+    section.appendChild(ul);
+    ul.appendChild(choice1);
+    ul.appendChild(choice2);
+    ul.appendChild(choice3);
+    ul.appendChild(choice4);
     
-    console.log("quesiton would appear")
+    questionTitle.textContent=questions[0].questionText
+    choice1.textContent=questions[0].choices[0]
+
+    
+
+    
+    
 }
 
 
-function countdownTimer(){
-    var countdown=setInterval(function(){
-        secondsLeft--;
-        if (secondsLeft===0){
-            clearInterval(secondsLeft)
-            // functino for the end of quiz here
-        }
-    },1000)
-}
 
 
-    
+var questions =[
+  {
+  
+    questionText: 'Placeholder question number one',
+    choices:['god','cat','unicorn','poiz'] ,
+    rightAwnswer:1 
+  },
+  {
+    questionText: 'Placeholder question number sjdfkjs',
+    choices:['god','cat','unicorn','poiz'] ,
+    rightAwnswer:1 
+  },
 
-
-
-var questions = [
-    {
-        question: "Placeholder question number one",
-        answers:[
-           a = "answer1",
-           b = "asnwer2",
-           c = "asnwer",
-           d = "asndwer",
-        ]
-        , correctAnswer: 'b'
-    },
-
-    {
-        question: "Question placeholder number two",
-        answers:[
-            a="answr",
-            b="asnwer",
-            c="asnwerd",
-            d="andkl",
-        ]
-        , correctAnswer:'a'
-    }
 ]
+console.log(questions[1].questionText)
 
 
-
-
+    function countdownTimer(){
+        var countdown=setInterval(function(){
+            secondsLeft--;
+            if (secondsLeft===0){
+                clearInterval(secondsLeft)
+                // functino for the end of quiz here
+            }
+        },1000)
+    }
+    
+    
+    
+    
